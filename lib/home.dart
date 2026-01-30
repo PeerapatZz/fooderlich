8a65caa7e018dd9fb1917af9d8b47d653adb0867
Peerapat Thinsorn <peerapat22647@gmail.com>
2026-01-30 14:20:53 +0700

    Finish commit message: Initial commit - Fooderlich app setup Card1

---
+++ lib/home.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/card1.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+int _selectedIndex = 0;
+
+static List<Widget> pages = <Widget>[
+  Card1(),
+  Container(color: Colors.green,),
+  Container(color: Colors.blue,),
+];
+
+void _onItemTapped(int index) {
+  setState(() {
+    _selectedIndex = index;
+  });
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Fooderlich',
+          // 2
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card 1',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card 2',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card 3',
+          ),
+        ],
+      ),
+    );
+  }
+}
